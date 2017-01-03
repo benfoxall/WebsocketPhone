dd4ae5ad0d866843c0d2b1756ccce0f22c2627e5
Ben Foxall <benfoxall@gmail.com>
2017-01-03 13:24:49 +0000

    Lots of server.js changes
    Main things:
    * websockets connections are manually accepted, allowing access to the
      requested path
    * paths/connections are grouped by conversation id
    * messages are proxied directly between browser and nexmo

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
 const fs = require('fs');
+const qs = require('querystring');
 const Nexmo = require('nexmo');
 const nexmo = new Nexmo({
   apiKey: process.env.API_KEY,
@@ -15,83 +17,115 @@ const nexmo = new Nexmo({
 });
 const wsServer = new WebSocketServer({
   httpServer: server,
-  autoAcceptConnections: true
+  autoAcceptConnections: false
 });
+const connections = new Map()
 
-let connectionsMap = new Map();
 
+// 1. a form for putting in a phone number
 app.get('/', (req, res, next) => {
-  fs.readFile('./index.html', function(error, data) {
-    res.writeHead(200);
-    res.end(data, 'utf-8');
-  });
-});
+  res.sendFile( __dirname + '/index.html')
+})
+
+// 2. handle a submitted number, start call to phone
+app.post('/call', (req, res, next) => {
+  const phoneNumber = req.body.number
+
+  console.log(`Attempt call to ${phoneNumber}`)
+
+  nexmo.calls.create({
+    to: [{
+      type: 'phone',
+      number: phoneNumber
+    }],
+    from: {
+      type: 'phone',
+      number: process.env.NEXMO_NUMBER
+    },
+    answer_url: [`https://${process.env.URL}/call/respond`]
+  }, (err, data) => {
+    if(err) return next(err)
+
+    res.redirect('/call?' + qs.stringify(data))
+  })
+})
+
+// 3. web user directed to holding page, connect to server & wait
+app.get('/call', (req, res, next) => {
+  res.sendFile( __dirname + '/call.html')
+})
+
+
+// 4. connect incoming call to server
+app.get('/call/respond', (req, res, next) => {
+  console.log("responding to ", req.query)
+
+  const conversation_uuid = req.query.conversation_uuid
+
+  res.send([
+    {
+      "action": "connect",
+      "eventUrl": [
+        `https://${process.env.URL}/events`
+      ],
+      "endpoint": [{
+        "type": "websocket",
+        "uri": `ws://${process.env.URL}/nexmo/${conversation_uuid}`,
+        "content-type": "audio/l16;rate=16000"
+      }]
+    }
+  ])
+})
+
+
+
 
 app.post('/events', (req, res, next) => {
-  console.log(req.body);
-  res.writeHead(200);
-  res.end('', 'utf-8');
+  console.log("EVENT", req.body)
+  res.sendStatus(200)
 });
 
-app.get('/ncco', (req, res, next) => {
-  console.log('Call for NCCO');
-  res.writeHead(200, {contentType: 'appliction/json'});
-  res.end(JSON.stringify([
-      {
-        "action": "talk",
-        "text": "Please hold"
-      },
-      {
-        "action": "connect",
-        "eventUrl": [
-          `https://${process.env.URL}/events`
-        ],
-        "from": req.query.phoneNumber,
-        "endpoint": [{
-          "type": "websocket",
-          "uri": `ws://${process.env.URL}`,
-          "content-type": "audio/l16;rate=16000"
-        }]
-      }
-    ]), 'utf-8');
-});
 
-wsServer.on('connect', function(connection) {
-  console.log('New connetion');
+wsServer.on('request', function(request) {
+
+  const connection = request.accept(null, request.origin)
+
+
+  // update the connection map
+  connections.set(request.resource, connection)
+  connection.on('close', function(reasonCode, description) {
+    connections.delete(request.resource)
+  })
+  console.log("connections: " + Array.from(connections.keys()))
+
+
+  // associate request with connection
+  connection.resource = request.resource
+  var re = /^\/(browser|nexmo)/
+  if(request.resource.match(re)) {
+    connection.other = request.resource.replace(re, str =>
+      str == '/nexmo' ? '/browser' : '/nexmo'
+    )
+  }
 
   connection.on('message', function(message) {
-    if (message.type === 'binary') {
-      // TODO: Send to call
 
-    } else if (message.utf8Data && JSON.parse(message.utf8Data).phoneNumber) {
-      let phoneNumber = JSON.parse(message.utf8Data).phoneNumber;
+    console.log('in> ', connection.resource)
 
-      connectionsMap.set(phoneNumber, {
-        browserConnection: connection,
-        phoneConnection: undefined
-      });
+    // proxy to other connection maybe
+    var other = connections.get(connection.other)
+    if(other) {
+      console.log('forward> ', connection.other)
 
-      console.log('Placing call to ' + phoneNumber);
-      nexmo.calls.create({
-        to: [{
-          type: 'phone',
-          number: phoneNumber
-        }],
-        from: {
-          type: 'phone',
-          number: process.env.NEXMO_NUMBER
-        },
-        answer_url: [`https://${process.env.URL}/ncco?phoneNumber=${phoneNumber}`]
-      }, (err, data) => {
-        console.log(err, data);
-      });
-
+      if(message.type == 'binary') {
+        other.sendBytes(message.binaryData)
+      } else {
+        other.send(message.utf8Data)
+      }
     }
+
   });
 
-  connection.on('close', function(reasonCode, description) {
-    console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
-  });
 });
 
 server.on('request', app);
